0f859713ac0c78ac28e6565e73b921522d54e81c
Cypher8 <seera.kaenkaew8@gmail.com>
2023-09-28 21:34:06 +0700

    feat: State management

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -9,10 +9,8 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
-    required this.onToggleFavorite,
   });
 
-  final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
@@ -25,7 +23,6 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
-          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
